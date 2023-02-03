ad56d81014e132b3f556e96da43e2fb3b128e35b
Gayatri <gayatrimane036@gmail.com>
2023-02-03 09:58:03 +0530

    test

---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello World in C");
+   printf("C programming is must for Firmware Programming");
+   return 0;
+}
